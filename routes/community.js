caf7da48b913fd6005e260d28b361690f27deaf4
sangkyulee <sklee7753@gmail.com>
2016-06-04 12:17:34 +0900

    DB update

--- routes/community.js
+++ routes/community.js
@@ -115,7 +115,61 @@ router.get('/:category', function(req, res, next) {
 });
 
 router.get('/:category/write', function(req, res, next) {
-	res.send('<script>alert("aaa")</script>;');
+	var category = req.params.category;
+
+	if (req.session.userno) {
+		var board_title;
+		if (category == 'Free') {
+			board_title = 'Free Topic';
+		} else if (category == 'Lecture') {
+			board_title = 'Lectures';
+		} else if (category == 'News') {
+			board_title = 'News & Magazines';
+		} else if (category == 'Notice') {
+			board_title = 'Notice';
+		} else {
+			conn.release();
+			res.redirect('/');
+		}
+		res.render('community_write', {board_title: board_title, category: category});
+	}	else {
+		res.redirect('/community/'+category);
+	}
+});
+
+router.post('/:category/insert', function(req, res, next) {
+	var category = req.params.category;
+
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		conn.query('use board');
+		conn.query('insert into Community_'+category+'(title, contents, writer_SN, date) values(?, ?, ?, now())', [req.body.title, req.body.content, req.session.userno], function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+			res.redirect('/community/'+category);
+		});
+	});
+});
+
+router.get('/:category/view', function(req, res, next) {
+	var category = req.params.category;
+
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		conn.query('use board');
+		conn.query('select SN as no, title, contents, (select nickname from Member where Member.SN=Community_'+category+'.writer_SN) writer, date_format(date, "%Y-%m-%d %H:%i:%s") date, views from Community_'+category+' where SN=?', [req.query.no], function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+			if (result)
+				res.render('community_view', {board_title: board_title, result: result[0]});
+			else
+				res.redirect('/community/'+category);
+		});
+	});
 });
 
 module.exports = router;
