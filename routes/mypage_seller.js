d41dca6f215138c8378c7627d0897f2f3c68a44a
ANNsujin <bgbanana4@gmail.com>
2016-06-06 17:05:46 +0900

    git re

--- routes/mypage_seller.js
+++ routes/mypage_seller.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var pool = mysql.createPool(
+/*var pool = mysql.createPool(
 {
 	host: 'localhost',
 	user: 'root',
 	password: '1234'
 }
-);
+);*/
 var urlencode = require('urlencode');
 
 router.get('/', function(req, res, next)
