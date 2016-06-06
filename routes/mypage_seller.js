960c33cefaece032e6380f6a274be5acb0193007
Ultimate-NEET <hoy9090@naver.com>
2016-06-06 05:36:00 +0000

    Userpage is made. getting information form database is required

--- routes/mypage_seller.js
+++ routes/mypage_seller.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mysql = mysql.createPool(
+{
+	host: 'localhost',
+	user: 'root',
+	password: '1234'
+}
+);
+var urlencode = require('urlencode');
 
 router.get('/', function(req, res, next)
 {
 	res.render('mypage_seller');
+
+	
+
 });
 
 module.exports = router;
