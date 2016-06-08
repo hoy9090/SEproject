39e926b1b323ae93b049cbc24c8b6d1404cd28a5
sangkyulee <sklee7753@gmail.com>
2016-06-08 20:59:13 +0900

    Edited app.js

--- routes/insert_product.js
+++ routes/insert_product.js
@@ -14,20 +14,22 @@ var md5 = require('md5');
 /* POST insert_product. */
 router.post('/', function(req, res, next) {
 	var image_path = md5(new Date()+'boardatoz');
-	var upload = multer({dest: 'public/images/products/'+image_path});
-	upload.array('file');
-	pool.getConnection(function(err, conn) {
-		conn.query('use board');
-		conn.query('insert into Product(type, subtype, name, price, stock, date, img_url, color, Seller_SN) values(?, ?, ?, ?, ?, now(), ?, ?, ?)', [req.body.type, req.body.subtype, req.body.name, req.body.price, req.body.stock, img_path, req.body.color, req.session.userno], function(err, result, field) {
-			if (err) {
-				console.error(err);
-			}
-			conn.release();
-			for (var index in req.files)
-				fs.rename('public/images/products/'+image_path+'/'+req.files[index].filename, 'public/images/products/'+image_path+'/'+index);
-			res.redirect('/');
-		});
-	});
+	var upload = multer({dest: 'public/images/products/'+image_path}).array('file');
+	upload(req_u, res_u, function (err) {
+		if (!err)
+	  	pool.getConnection(function(err, conn) {
+				conn.query('use board');
+				conn.query('insert into Product(type, subtype, name, price, stock, date, img_url, color, Seller_SN) values(?, ?, ?, ?, ?, now(), ?, ?, ?)', [req.body.type, req.body.subtype, req.body.name, req.body.price, req.body.stock, img_path, req.body.color, req.session.userno], function(err, result, field) {
+					if (err) {
+						console.error(err);
+					}
+					conn.release();
+					for (var index in req.files)
+						fs.rename('public/images/products/'+image_path+'/'+req.files[index].filename, 'public/images/products/'+image_path+'/'+index);
+					res.redirect('/');
+				});
+			});  
+  });
 });
 
 module.exports = router;
