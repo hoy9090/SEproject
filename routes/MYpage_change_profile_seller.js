7c73011b7d194e3a70e3c1b64bd735ee6ba40975
sangkyulee <sklee7753@gmail.com>
2016-06-08 21:30:16 +0900

    Edited mypage

--- routes/MYpage_change_profile_seller.js
+++ routes/MYpage_change_profile_seller.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1234'
+});
 
-/* GET about. */
+/* GET MYpage_change_profile_seller. */
 router.get('/', function(req, res, next) {
-	res.render('MYpage_change_profile_seller');
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		conn.query('use board');
+		conn.query('select name, nickname, phone_number, address, corp_num from Member where SN='+req.session.userno, function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+			res.render('MYpage_change_profile_seller', {info: result[0]});
+		});
+	});
 });
 
 module.exports = router;
