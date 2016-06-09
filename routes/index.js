2ffef2f9693a1895de9269b7320800fe6e1e5058
jeffers9590 <jeffers9590@gmail.com>
2016-06-09 12:09:09 +0900

    go back

--- routes/index.js
+++ routes/index.js
@@ -8,44 +8,7 @@ var pool = mysql.createPool({
 });
 
 /* GET index. */
-router.get('/', function(req, res, next) {  
-	var type = req.query.type;
-	var subtype = req.query.subtype;
-	var typeString;
-	if (type == 0) {
-		if (subtype == 0) {
-			typeString = "Longboard";
-		} else if (subtype == 1) {
-			typeString = "Cruserboard";
-		} else if (subtype == 2) {
-			typeString = "Skateboard";
-		}
-	} else if (type == 1) {
-		if (subtype == 0) {
-			typeString = "Truck";
-		} else if (subtype == 1) {
-			typeString = "Wheel";
-		} else if (subtype == 2) {
-			typeString = "Deck";
-		}
-	} else if (type == 2) {
-		if (subtype == 0) {
-			typeString = "Cleaning";
-		} else if (subtype == 1) {
-			typeString = "Protects";
-		}
-	}
-	pool.getConnection(function(err, conn) {
-		if (err)
-			console.error(err);
-		conn.query('use board');
-		conn.query('select SN, name, price, img_url from Product where type=? and subtype=? order by SN desc', [type, subtype], function(err, result, field) {
-			if (err)
-				console.error(err);
-			conn.release();
-			res.render('index', {list: result, type: typeString});
-		});
-	});
+router.get('/', function(req, res, next) {
 	res.render('index');
 });
 
