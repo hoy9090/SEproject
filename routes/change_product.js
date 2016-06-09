943d1f12812ce20100ebc327766b69d68bbbdb92
ANNsujin <bgbanana4@gmail.com>
2016-06-10 00:18:27 +0900

    stock modification update

--- routes/change_product.js
+++ routes/change_product.js
@@ -7,29 +7,31 @@ var pool = mysql.createPool({
 	password: '1234'
 });
 
-/* POST change_profile. */
+/* POST change_product. */
 router.post('/', function(req, res, next)
 {
 	pool.getConnection(function(err, conn) {
 		if (err)
 			console.error(err);
 		conn.query('use board');
-		var nickname = req.body.nickname;
-		var phone_number = req.body.phone_number;
-		var address = req.body.address;
-		var corp_num = req.body.corp_num;
 		var queryString = "";
-		if (nickname != '')
-			queryString = queryString.concat("nickname='"+nickname+"', ");
-		if (phone_number != '')
-			queryString = queryString.concat("phone_number='"+phone_number+"', ");
-		if (address != '')
-			queryString = queryString.concat("address='"+address+"', ");
-		if (corp_num != '')
-			queryString = queryString.concat("corp_num='"+corp_num+"', ");
+		if (type != '')
+			queryString = queryString.concat("nickname='"+type+"', ");
+		if (subtype != '')
+			queryString = queryString.concat("nickname='"+subtype+"', ");
+		if (name != '')
+			queryString = queryString.concat("nickname='"+name+"', ");
+		if (price != '')
+			queryString = queryString.concat("phone_number='"+price+"', ");
+		if (stock != '')
+			queryString = queryString.concat("address='"+stock+"', ");
+		if (img_url != '')
+			queryString = queryString.concat("corp_num='"+img_url+"', ");
+		if (color != '')
+			queryString = queryString.concat("corp_num='"+color+"', ");
 		queryString = queryString.slice(0, -2);
 		console.log(queryString);
-		conn.query('update Member set '+queryString+' where SN='+req.session.userno, function(err, result, field) {
+		conn.query('update Product set '+queryString+' where SN='+req.session.userno, function(err, result, field) {
 			conn.release();
 			if (err) {
 				console.error(err);
