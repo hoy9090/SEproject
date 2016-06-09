a7721c354443039a6041c94ca048301687891736
Jeonghun <jhk4148@naver.com>
2016-06-09 09:26:38 -0700

    thread edting

--- routes/thread.js
+++ routes/thread.js
@@ -1,37 +1,31 @@
+
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var pool = mysql.createPool
-({
+var pool = mysql.createPool({
 	host: 'localhost',
 	user: 'root',
 	password: '1234'
-
 });
-
 
 router.get('/', function(req, res, next)
 {
-	var threadno = req.query.threadno;
+	var type = req.query.type;
+	var subtype = req.query.subtype;
+	var typeString;
 
-	pool.getConnection(function(err, conn)
-	{
-		if(err)
+	pool.getConnection(function(err, conn) {
+		if (err)
 			console.error(err);
-
-		conn.query('use deal');
-		conn.query('select date, buyer_comment, seller_comment where SN=?', [threadno], function(err, result, field)
-		{
-			if(err)
+		conn.query('use board');
+		conn.query('select buyer_comment from deal where SN=? order by SN desc', [type], function(err, result, field) {
+			if (err)
 				console.error(err);
 			conn.release();
-			res.render('thread', {list:result});
-
+			res.render('thread', {list: result, type: typeString});
 		});
-
 	});
-
-	res.render('thread');	
 });
+
 
 module.exports = router;
