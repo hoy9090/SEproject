b610b3f57da04dff09ccc8c4d0e95f6e79a5983b
sangkyulee <sklee7753@gmail.com>
2016-06-09 21:21:14 +0900

    index.jade

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,42 @@ var pool = mysql.createPool({
 
 /* GET index. */
 router.get('/', function(req, res, next) {
-	res.render('index');
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		conn.query('use board');
+		conn.query('select SN, name, type, subtype, price, img_url from Product order by SN desc limit(0, 4)', function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+			for (var i=0; i<result.length; i++) {
+				if (result[i].type == 0) {
+					if (result[i].subtype == 0) {
+						result[i].typeString = "Longboard";
+					} else if (result[i].subtype == 1) {
+						result[i].typeString = "Cruserboard";
+					} else if (result[i].subtype == 2) {
+						result[i].typeString = "Skateboard";
+					}
+				} else if (result[i].type == 1) {
+					if (result[i].subtype == 0) {
+						result[i].typeString = "Truck";
+					} else if (result[i].subtype == 1) {
+						result[i].typeString = "Wheel";
+					} else if (result[i].subtype == 2) {
+						result[i].typeString = "Deck";
+					}
+				} else if (result[i].type == 2) {
+					if (result[i].subtype == 0) {
+						result[i].typeString = "Cleaning";
+					} else if (result[i].subtype == 1) {
+						result[i].typeString = "Protects";
+					}
+				}
+			}
+			res.render('index', {list: result});
+		});
+	});
 });
 
 module.exports = router;
