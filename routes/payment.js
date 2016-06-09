cfe1a7155671bc9385a63ca00694a2c311cf2fa7
sangkyulee <sklee7753@gmail.com>
2016-06-10 01:50:41 +0900

    shopping cart

--- routes/payment.js
+++ routes/payment.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res, next)
-{
-	res.render('payment');
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1234'
 });
 
-router.post('/', function(req, res, next) {
-  res.render('payment', {email: req.body.email, name: req.body.name});
+/* GET payment. */
+router.get('/', function(req, res, next)
+{
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		conn.query('use board');
+		conn.query('select SN, amount, product_SN as no, (select name from Product where Product.SN=Shopping_cart.product_SN) as name, (select img_url from Product where Product.SN=Shopping_cart.product_SN) as img_url, (select price from Product where Product.SN=Shopping_cart.product_SN) as price from Shopping_cart where Shopping_cart.buyer_SN=?', [req.session.userno], function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+		  res.render('payment', {list: result});
+		});
+	});
 });
 
 module.exports = router;
