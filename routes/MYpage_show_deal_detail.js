0a719dc4828637d8ce57907369d92dd3ffa93173
ANNsujin <bgbanana4@gmail.com>
2016-06-10 01:47:14 +0900

    mypage connection  update

--- routes/MYpage_show_deal_detail.js
+++ routes/MYpage_show_deal_detail.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1234'
+});
 
-/* GET MYpage_show_deal. */
+/* GET MYpage_show_deal_detail */
 router.get('/', function(req, res, next) {
-	res.render('MMYpage_show_deal_detail');
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		var isBuyer = req.session.isBuyer;
+		var queryString;
+		if (isBuyer)
+			queryString = 'buyer_SN';
+		else
+			queryString = 'seller_SN';
+		conn.query('use board');
+		conn.query('select SN, order_SN, date, seller_comment, buyer_comment from Deal (select '+queryString+' from `Order` where Deal.order_SN=`Order`.SN)='+req.session.userno, function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+			res.render('MYpage_show_deal_detail', {info: result[0]});
+		});
+	});
 });
 
 module.exports = router;
