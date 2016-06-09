9d020170abc44f6ef50d5390c3485fbee7a8710f
ANNsujin <bgbanana4@gmail.com>
2016-06-09 23:20:03 +0900

    stock modify

--- routes/MYpage_show_stock.js
+++ routes/MYpage_show_stock.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1234'
+});
 
 /* GET MYpage_show_stock */
 router.get('/', function(req, res, next) {
-	res.render('MYpage_show_stock');
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		conn.query('use board');
+		conn.query('select SN, name from Product', function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+			res.render('MYpage_show_stock', {contents: result});
+		});
+	});
 });
 
 module.exports = router;
