f80c5b96d116679d89a63eaae688b0e449d09c98
sangkyulee <sklee7753@gmail.com>
2016-06-10 00:37:28 +0900

    shopping cart

--- routes/shopping_cart.js
+++ routes/shopping_cart.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
 		if (err)
 			console.error(err);
 		conn.query('use board');
-		conn.query('select SN, amount, product_SN as no, (select name from Product where Product.SN=Shopping_cart.product_SN) as name, (select img_url from Product where Product.SN=Shopping_cart.product_SN) as img_url from Shopping_cart where Shopping_cart.buyer_SN=?', [req.session.userno], function(err, result, field) {
+		conn.query('select SN, amount, product_SN as no, (select name from Product where Product.SN=Shopping_cart.product_SN) as name, (select img_url from Product where Product.SN=Shopping_cart.product_SN) as img_url, (select price from Product where Product.SN=Shopping_cart.product_SN) as price from Shopping_cart where Shopping_cart.buyer_SN=?', [req.session.userno], function(err, result, field) {
 			if (err)
 				console.error(err);
 			conn.release();
