a0a2992adf1099009f7a7dc8ad2cdf84c81d2e9e
ANNsujin <bgbanana4@gmail.com>
2016-06-10 04:52:19 +0900

    mypage connection  update

--- routes/change_bid_candidate.js
+++ routes/change_bid_candidate.js
@@ -14,14 +14,16 @@ router.post('/', function(req, res, next)
 		if (err)
 			console.error(err);
 		conn.query('use board');
-		console.log(req.body);
 		var SN = req.body.NO;
-		var total_price = req.body.total_price;
+		var price = req.body.price;
+		var comment = req.body.comment;
 		var queryString = "";
-		if (total_price != null)
-			queryString = queryString.concat("total_price='"+total_price+"', ");
+		if (price != null)
+			queryString = queryString.concat("price='"+price+"', ");
+		if (comment != null)
+			queryString = queryString.concat("comment='"+comment+"', ");
 		queryString = queryString.slice(0, -2);
-		conn.query('update `Order` set '+queryString+' where `Order`.seller_SN='+req.session.userno, function(err, result, field) {
+		conn.query('update Bid_candidate set '+queryString+' where seller_SN='+SN, function(err, result, field) {
 			conn.release();
 			if (err) {
 				console.error(err);
