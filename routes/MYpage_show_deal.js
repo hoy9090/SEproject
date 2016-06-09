3c043825f9fe9c21ab0faf0de83334ad5ffee68e
ANNsujin <bgbanana4@gmail.com>
2016-06-10 00:33:17 +0900

    stock modification update

--- routes/MYpage_show_deal.js
+++ routes/MYpage_show_deal.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
 		if (err)
 			console.error(err);
 		conn.query('use board');
-		conn.query('select SN, date from Deal', function(err, result, field) {
+		conn.query('select SN, date from Deal where SN='+req.session.userno, function(err, result, field) {
 			if (err)
 				console.error(err);
 			conn.release();
--- routes/MYpage_show_stock_detail.js
+++ routes/MYpage_show_stock_detail.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1234'
+});
 
 /* GET MYpage_show_stock_detail */
 router.get('/', function(req, res, next) {
-	res.render('MYpage_show_stock_detail');
+	pool.getConnection(function(err, conn) {
+		if (err)
+			console.error(err);
+		conn.query('use board');
+		conn.query('select SN, type, subtype, name, price, stock, date, img_url, color from Product where SN='+req.session.userno, function(err, result, field) {
+			if (err)
+				console.error(err);
+			conn.release();
+			res.render('MYpage_show_stock_detail', {info: result[0]});
+		});
+	});
 });
 
 module.exports = router;
