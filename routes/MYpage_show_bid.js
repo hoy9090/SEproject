e99cf1d5d87c5536b55e911c34f6d9e1de788eb2
junkev7 <junkev7@gmail.com>
2016-06-10 06:10:11 +0000

    submenus in mypage edited

--- routes/MYpage_show_bid.js
+++ routes/MYpage_show_bid.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
 		if (err)
 			console.error(err);
 		conn.query('use board');
-		conn.query('select SN from Bid_candidate where SN='+req.query.number, function(err, result, field) {
+		conn.query('select * from Bid_candidate where seller_SN='+req.session.userno, function(err, result, field) {
 			if (err)
 				console.error(err);
 			conn.release();
