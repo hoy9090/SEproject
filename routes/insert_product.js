fa04e3c992341b0416f361496d3bd899bb79bd3d
sangkyulee <sklee7753@gmail.com>
2016-06-10 18:21:54 +0900

    payment

--- routes/insert_product.js
+++ routes/insert_product.js
@@ -20,17 +20,15 @@ router.post('/', function(req, res, next) {
 	  	pool.getConnection(function(err, conn) {
 				conn.query('use board');
 				conn.query('insert into Product(type, subtype, name, price, stock, date, color, Seller_SN) values(?, ?, ?, ?, ?, now(), ?, ?)', [req.body.type, req.body.subtype, req.body.name, req.body.price, req.body.stock, req.body.color, req.session.userno], function(err, result, field) {
-					if (err) {
+					if (err)
 						console.error(err);
-					}
 					var filename = [0, 0];
 					for (var index in req.files) {
 						filename[index] = image_path+'/'+req.files[index].filename;
 					}
 					conn.query('select SN from Product order by SN desc limit 0, 1', function(err, result, field) {
-						if (err) {
+						if (err)
 							console.error(err);
-						}
 						var SN = result[0].SN;
 						conn.query('update Product set img_url=?, detail_image_url=? where SN='+SN, [filename[0], filename[1]], function(err, result,field) {
 							if (err)
