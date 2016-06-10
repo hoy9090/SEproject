35d6240cee6491f939763eacf497745697fbe018
sangkyulee <sklee7753@gmail.com>
2016-06-10 19:50:01 +0900

    customize

--- routes/get_board.js
+++ routes/get_board.js
@@ -12,11 +12,11 @@ router.post('/', function(req, res, next) {
 	pool.getConnection(function(err, conn) {
 		if (err)
 			console.error(err);
-		var type = req.body.type;
+		var type = parseInt(req.body.type);
 		var subtype = req.body.subtype;
 
 		conn.query('use board');
-		conn.query('select SN, price, img_url from Product where type=1 and subtype='+subtype, function(err, result, field) {
+		conn.query('select SN, price, img_url, subtype as `type` from Product where type=1 and subtype='+subtype, function(err, result, field) {
 			if (err) {
 				console.error(err);
 				conn.release();
